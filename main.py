512c685d31153f2901482599aaaff38f25907213
blast <rodfval.fv@gmail.com>
2024-09-06 14:02:46 +0200

    increased logging and modified env vars

--- main.py
+++ main.py
@@ -21,6 +21,8 @@ client_secret=os.environ.get("SPOTIPY_CLIENT_SECRET")
 redirect_uri=os.environ.get("SPOTIPY_REDIRECT_URI")
 scope="user-top-read,user-library-read,user-read-recently-played"
 
+logging.info('Process Start!')
+
 creds = SpotifyOAuth(scope=scope, client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri, open_browser=False)
 sp = spotipy.Spotify(auth_manager=creds)
 
@@ -45,16 +47,20 @@ def delete_old_playlists_from_navidrome():
 
 def my_reccommendations():
     try:
-        for i in range(5):
-            top_tracks = sp.current_user_top_tracks(limit=50, time_range='long_term')
-            liked_tracks = sp.current_user_saved_tracks(limit=50)
-            history = sp.current_user_recently_played(limit=50)
+        top_tracks = sp.current_user_top_tracks(limit=50, time_range='long_term')
+        logging.info('Loaded your custom top tracks')
+        liked_tracks = sp.current_user_saved_tracks(limit=50)
+        logging.info('Loaded your top liked tracks')
+        history = sp.current_user_recently_played(limit=50)
+        logging.info('Loaded your played tracks')
+        for i in range(int(os.environ.get("NUM_USER_PLAYLISTS"))):
+            logging.info('Searching your reccomendations (playlist %s)', str(i+1))
             top_track_ids = [track['id'] for track in top_tracks['items']]
             liked_track_ids = [track['track']['id'] for track in liked_tracks['items']]
             history_track_ids = [track['track']['id'] for track in history['items']]
             seed_track_ids = top_track_ids + liked_track_ids + history_track_ids
             random.shuffle(seed_track_ids)
-            results = sp.recommendations(seed_tracks=seed_track_ids[0:5], limit=100)
+            results = sp.recommendations(seed_tracks=seed_track_ids[0:5], limit=int(os.environ.get("ITEMS_PER_PLAYLIST")))
             write_reccomandation_file(playlist_dir + "/" + "My Reccomendations - " + str(i+1) + ".m3u", results)
     except Exception as e:
       exc_type, exc_obj, exc_tb = sys.exc_info()
@@ -73,6 +79,8 @@ def write_reccomandation_file(playlist_path_file, results):
     try:
         reccomendations_array = []
         for track in results['tracks']:
+            
+            logger.debug('Searching %s - %s in your music library (%s)', track['name'], track['artists'][0]['name'], music_dir)
             artist_path_dir = None
             for artist_name in os.listdir(music_dir):
                 if artist_name.lower() ==  track['artists'][0]['name'].lower() or artist_name.lower() in track['artists'][0]['name'].lower() or track['artists'][0]['name'].lower() in artist_name.lower():
@@ -92,16 +100,17 @@ def write_reccomandation_file(playlist_path_file, results):
             with open(playlist_path_file, 'a') as opened_playlist_file:
                 for reccomendation in reccomendations_array:
                     opened_playlist_file.write(reccomendation + '\n')
-                    logging.info('Recommendation: %s - %s', track['name'],
-                                track['artists'][0]['name'])
+                    logging.debug('Writing reccomendation %s to playlist (%s)', reccomendation, playlist_path_file)
+
     except Exception as e:
       exc_type, exc_obj, exc_tb = sys.exc_info()
       fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
       logging.error("%s %s %s", exc_type, fname, exc_tb.tb_lineno, exc_info=1)
 
 def show_recommendations_for_artist(name):
+    logging.info('Searching reccomendations for: %s', name)
     artist = get_artist(name)
-    results = sp.recommendations(seed_artists=[artist['id']], limit=100)
+    results = sp.recommendations(seed_artists=[artist['id']], limit=int(os.environ.get("ITEMS_PER_PLAYLIST")))
     write_reccomandation_file(playlist_dir + "/" + name + " - Reccomendations.m3u", results)
 
 delete_old_playlists_from_navidrome()
@@ -110,3 +119,6 @@ my_reccommendations()
 
 for artist_name in os.listdir(music_dir):
   show_recommendations_for_artist(artist_name)
+
+
+logging.info('Process Done!')
