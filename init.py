288ab2856ae64da6a68afb040cdf29fa00ff3bc9
blast <rodfval.fv@gmail.com>
2024-09-09 16:25:38 +0200

    added init.py

---
+++ init.py
@@ -0,0 +1,15 @@
+import spotipy  
+import os
+from spotipy import SpotifyOAuth
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+client_id=os.environ.get("SPOTIPY_CLIENT_ID")
+client_secret=os.environ.get("SPOTIPY_CLIENT_SECRET")
+redirect_uri=os.environ.get("SPOTIPY_REDIRECT_URI")
+scope="user-top-read,user-library-read,user-read-recently-played"
+
+creds = SpotifyOAuth(scope=scope, client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri, open_browser=False)
