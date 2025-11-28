f8062162c914947a05dcb48c477a428b872c9e8b
Tony <18640201+Tonemon@users.noreply.github.com>
2025-11-28 11:13:55 +0100

    circular ImportError temporary fix

--- spotisub/__init__.py
+++ spotisub/__init__.py
@@ -1,5 +1,4 @@
 """Spotisub init module"""
-from spotisub import routes, classes, errors
 import logging
 import os
 from logging.handlers import RotatingFileHandler
@@ -42,3 +41,5 @@ bootstrap = Bootstrap(spotisub)
 configuration_db = SQLAlchemy(spotisub)
 login = LoginManager(spotisub)
 login.login_view = 'login'
+
+from . import routes, classes, errors
